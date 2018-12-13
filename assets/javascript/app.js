7c21da1547a30e5b0fcd2661632238cefdfecd79
Kylee Morris <kyleemorris21@gmail.com>
2018-12-12 20:17:19 -0700

    pushed files

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// varibles
+var correct = 0;
+var wrong = 0;
+var time = 45;
+var timer = time;
+
+function displayQuestions() {
+    $("game").empty()
+    time = $("<h1>")
+    console.log(time);
+}
